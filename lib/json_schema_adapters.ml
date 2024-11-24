e7958526251e09c50004cd90fc1e9b3e2d1ef69d
Seb Mondet <sebastien.mondet@ahrefs.com>
2024-11-24 17:16:53 -0500

    Make `"additionalProperties"` “boolable”

--- lib/json_schema_adapters.ml
+++ lib/json_schema_adapters.ml
@@ -9,4 +9,15 @@ module Or_ref = struct
     | x -> x
 end
 
+module Or_bool = struct
+  let normalize : Yojson.Safe.t -> Yojson.Safe.t = function
+    | `Bool b -> `List [ `String "Bool"; `Bool b ]
+    | obj -> `List [ `String "Obj"; obj ]
+
+  let restore = function
+    | `List [ `String "Bool"; `Bool b ] -> `Bool b
+    | `List [ `String "Obj"; obj ] -> obj
+    | x -> x
+end
+
 module Ref = Utils.Fresh (String) ()
